8b370852dde445a21bc4d396036258359f2dc94e
leovante <leovante@live.ru>
2018-03-21 22:53:41 +0300

    first

--- src/main/java/ItBooksTests.java
+++ src/main/java/ItBooksTests.java
@@ -23,7 +23,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.BooksSite;
-
+//1
 public class ItBooksTests {
     WebDriver webDriver;
     BooksSite website;
