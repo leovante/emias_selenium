937b8ef1d60b18bbc1e265a187b58eba01901b09
Leovante <LotusEclipse88>
2018-03-28 12:15:00 +0300

    пк на работе, обед третий кейс

--- src/main/java/EMIAStest.java
+++ src/main/java/EMIAStest.java
@@ -16,10 +16,14 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.EmiasSite;
+
+import java.util.List;
 
 public class EMIAStest {
     WebDriver webDriver;
@@ -64,7 +68,58 @@ public class EMIAStest {
 
     @Test
     public void  testVedenieRaspisaniya() throws InterruptedException {
+        System.out.println("KEYS 3: Enter in doctor's schedule and create task");
 
+
+        //заходим в расписание
+        webDriver.findElement(By.xpath("//div[@id='Portlet_2']/div[2]/div[2]/a/span")).click();
+        //получаем список врачей
+
+
+        //webDriver.findElement(By.linkText(bookTitle)).click();
+        List<WebElement> doctorList = webDriver.findElement(By.id("schw_docprvdgrid1"))
+                .findElements(By.tagName("tr"));
+        for (WebElement oneDoctor : doctorList) {
+            WebElement doctor = oneDoctor.findElement(By.tagName("role"));
+        //тут выбрать врача и ссылку на метод который делает расписание
+
+//            if (doctor.getLocation() getTagName("tr").contains("row")) {
+//                doctor.click();
+//                break;
+//            }
+
+        }
+
+        //нажимаем на флажок врача
+        webDriver.findElement(By.id("jqg_schw_docprvdgrid1_4398")).click();
+        //нажимаем на создать расписание
+        webDriver.findElement(By.xpath("//button[@id='btn_create']/span[2]")).click();
+        //нажимаем на поле начала интервала
+        webDriver.findElement(By.id("pickTime_nach")).click();
+        //нажали закрыть календарь
+        webDriver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
+        //нажимаем на поле окончание интервала
+        webDriver.findElement(By.id("pickTime_okon")).click();
+        //нажали закрыть календарь
+        webDriver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
+        //нажимаем на выпадающий список тип приема
+        webDriver.findElement(By.xpath("//a[@id='ddlbusytype-button']/span[2]")).click();
+        //выбор подменю
+        webDriver.findElement(By.id("ui-selectmenu-item-113")).click();
+        //нажали кнопу добавить
+        webDriver.findElement(By.id("schedule_add_button")).click();
+        //нажимаем кнопку сохранить
+        webDriver.findElement(By.xpath("//button[@id='btn_save_schedule']/span")).click();
+        //подтверждаем предупреждение кнопкой - да
+        webDriver.findElement(By.xpath("//div[24]/div[3]/div/button/span")).click();
+
+        //тут нужно сделать условие, если появилось окошко с ошибкой,то нажимаем - закрыть
+        webDriver.findElement(By.xpath("//div[24]/div[3]/div/button/span")).click();
+        //нажимаем отмена
+        webDriver.findElement(By.xpath("//div/button[3]/span")).click();
+        //дальше нужно перейти на другого врача из списка и сделать всё тоже самое
+        /*     */
+        //после создания записи, нужно проверить что появилась ячейка на это время с этим цветом
 
 /*
 -открыть емиас http://emias.mosreg.ru/mis/test_emias
