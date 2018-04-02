376addfdae0eb875a749ccf8cc95ffc947561f33
leovante <leovante@live.ru>
2018-04-02 20:10:45 +0300

    сделал шаблоны для теста создания нового вызова и теста создания мкаб

--- src/main/java/EMIAStest.java
+++ src/main/java/EMIAStest.java
@@ -66,7 +66,7 @@ public class EMIAStest {
         logger.fatal("Before: Done");
     }
 
-    @Test
+    @Test//
     public void testSearchCallDoctor() throws InterruptedException {
         logger.info("KEYS 1: Enter CallDoctor Page");
         website.emiasPage().clickCallDoctorButton();
