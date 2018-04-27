ab2fe49278c9a1c1965c5adc90ae39e97b13e1b1
Leovante <LotusEclipse88>
2018-04-27 17:41:26 +0300

    нужно вычищать записи из бд перед проверками

--- src/main/java/EMIAStest.java
+++ src/main/java/EMIAStest.java
@@ -14,44 +14,35 @@
 *
 * */
 
-import org.apache.log4j.Level;
-import org.apache.log4j.PropertyConfigurator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.*;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import test.EmiasSite;
 import org.apache.log4j.Logger;
+import test.sql.SqlMethods;
 
 public class EMIAStest {
-    WebDriver webDriver;
+    private WebDriver webDriver;
     EmiasSite website;
     WebDriverWait wait;
     Logger logger;
 
     @Before
     public void setUp() {
-        logger = Logger.getLogger("emias_selenium");
-        PropertyConfigurator.configure("src/main/resources/log4j.properties");
-        logger.setLevel(Level.FATAL);
-//        LoggingPreferences logs = new LoggingPreferences();
-//        logs.enable(LogType.BROWSER, Level.ALL);
-//        logs.enable(LogType.CLIENT, Level.ALL);
-//        logs.enable(LogType.DRIVER, Level.ALL);
-//        logs.enable(LogType.PERFORMANCE, Level.ALL);
-//        logs.enable(LogType.PROFILER, Level.ALL);
-//        logs.enable(LogType.SERVER, Level.ALL);
-//
-//        DesiredCapabilities desiredCapabilities = DesiredCapabilities.chrome();
-//        desiredCapabilities.setCapability(CapabilityType.LOGGING_PREFS, logs);
+        //PropertyConfigurator.configure("src/main/resources/log4j.properties");
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        webDriver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        webDriver = new ChromeDriver(options);
+
         wait = new WebDriverWait(webDriver, 60, 500);
         website = new EmiasSite(webDriver);
-        //webDriver.manage().window().maximize();
+
         webDriver.get("http://emias.mosreg.ru/mis/test_emias");
         website.loginPage().enterLoginText("seleniumAdmin");
         website.loginPage().enterPasswordText("1212");
@@ -60,15 +51,31 @@ public class EMIAStest {
         website.loginPage().clickLoginButton();
     }
 
+    @Test// KEYS 0.0
+    public void testProject() throws InterruptedException {
+        SqlMethods sqlMethod = new SqlMethods();
+        String name = "Аблова";
+        sqlMethod.deleteShedule(name);
+
+
+
+
+    }
+
+
+
     @Test// KEYS 1.1
     public void testCreateShedule() throws InterruptedException {
-        logger.info("KEYS 1.1: Enter doctor's timetable");
+
         website.emiasPage().clickTimeTable();
         String docName = website.scheduleDoctors().getUnicalDoctor(null);
+
+        //тут запрос на удаление расписания у врача
+
+
         website.scheduleDoctors().selectDoctor(docName);
         website.scheduleDoctors().createShedule();
         website.scheduleDoctors().checkCreateShedule();
-        logger.error("KEYS 1.1: Done");
     }
 
     @Test//KEYS 1.2
@@ -184,60 +191,60 @@ public class EMIAStest {
          */
     }
 
-/*KEYS 1.5 Перенос записей
-2. Перешли на вкалдку ""Перенос записей"" :http://emias.mosreg.ru/mis/test_emias/ScheduleWriting/Transfer
-3. Выбираем врача у которого есть записи пациентов в расписании(или предварительно записываем пациента на данного врача)"
-
-
-1. Осуществляем поиск врача
-    - Выбрать критерий поиска врача, например, ФИО
-    - Ввести в поле поиска данные, по которым будет производиться поиск врача (для поиска ввести не менее трех символов)
-    - Нажать кнопку «Найти»
-2. Нажимаем ""Перенос записей""
-3. Указываем начала и конец интервала
-4. Выбираем врача
-5. Нажима ПКМ на запись которую будем переносить и выюираем ""Перенести запись""
-6. Указываем период расписания на которое будет переноситься запись
-7. Выбираем врача и ячейку на которую будем переносить запись
-8. Нажимаем ""Перенести""
-*/
-
-/*
-    @Test//
-    public void testSearchCallDoctor() throws InterruptedException {
-        logger.info("KEYS 1: Search call calldoctor page");
-        website.emiasPage().clickCallDoctorButton();
-        website.callDoctorPage().clickCallDoctorSearchBtn();
-        website.callDoctorPage().waitForSearchResults();
-        logger.info("KEYS 1: Done");
-    }
-
-    @Test//
-    public void testSearchCallDoctorWithFilter() throws InterruptedException {
-        logger.info("KEYS 2: Search call calldoctor page with filter");
-        website.emiasPage().clickCallDoctorButton();
-        website.callDoctorPage().filterCallDoctor();
-        website.callDoctorPage().clickCallDoctorSearchBtn();
-        website.callDoctorPage().verificationTableGridNull();
-        logger.info("KEYS 2: Done");
-    }
-
-    @Test//
-    public void testCreateNewCall_ExistingMkab() throws InterruptedException {
-        logger.info("KEYS 4: Enter CallDoctor");
-        website.emiasPage().clickCallDoctorButton();
-        website.moduleCallDoctor_CreateCall().createNewCall_ExistingMkab();
-        logger.error("KEYS 4: Done");
-    }
-*/
-/*    @Test// 5
-    public void  testCreateNewMkab() throws InterruptedException {
-        logger.info("KEYS 5: Enter in CallDoctor");
-        website.emiasPage().clickCallDoctorButton();
-        website.callDoctorPage().createNewCall();
-        logger.error("KEYS 5: Done");
-    }
-*/
+///*KEYS 1.5 Перенос записей
+//2. Перешли на вкалдку ""Перенос записей"" :http://emias.mosreg.ru/mis/test_emias/ScheduleWriting/Transfer
+//3. Выбираем врача у которого есть записи пациентов в расписании(или предварительно записываем пациента на данного врача)"
+//
+//
+//1. Осуществляем поиск врача
+//    - Выбрать критерий поиска врача, например, ФИО
+//    - Ввести в поле поиска данные, по которым будет производиться поиск врача (для поиска ввести не менее трех символов)
+//    - Нажать кнопку «Найти»
+//2. Нажимаем ""Перенос записей""
+//3. Указываем начала и конец интервала
+//4. Выбираем врача
+//5. Нажима ПКМ на запись которую будем переносить и выюираем ""Перенести запись""
+//6. Указываем период расписания на которое будет переноситься запись
+//7. Выбираем врача и ячейку на которую будем переносить запись
+//8. Нажимаем ""Перенести""
+//*/
+//
+///*
+//    @Test//
+//    public void testSearchCallDoctor() throws InterruptedException {
+//        logger.info("KEYS 1: Search call calldoctor page");
+//        website.emiasPage().clickCallDoctorButton();
+//        website.callDoctorPage().clickCallDoctorSearchBtn();
+//        website.callDoctorPage().waitForSearchResults();
+//        logger.info("KEYS 1: Done");
+//    }
+//
+//    @Test//
+//    public void testSearchCallDoctorWithFilter() throws InterruptedException {
+//        logger.info("KEYS 2: Search call calldoctor page with filter");
+//        website.emiasPage().clickCallDoctorButton();
+//        website.callDoctorPage().filterCallDoctor();
+//        website.callDoctorPage().clickCallDoctorSearchBtn();
+//        website.callDoctorPage().verificationTableGridNull();
+//        logger.info("KEYS 2: Done");
+//    }
+//
+//    @Test//
+//    public void testCreateNewCall_ExistingMkab() throws InterruptedException {
+//        logger.info("KEYS 4: Enter CallDoctor");
+//        website.emiasPage().clickCallDoctorButton();
+//        website.moduleCallDoctor_CreateCall().createNewCall_ExistingMkab();
+//        logger.error("KEYS 4: Done");
+//    }
+//*/
+///*    @Test// 5
+//    public void  testCreateNewMkab() throws InterruptedException {
+//        logger.info("KEYS 5: Enter in CallDoctor");
+//        website.emiasPage().clickCallDoctorButton();
+//        website.callDoctorPage().createNewCall();
+//        logger.error("KEYS 5: Done");
+//    }
+//*/
 
     @After
     public void tearDown() {
@@ -250,11 +257,7 @@ public class EMIAStest {
         if (webDriver != null)
             webDriver.quit();
 
-        logger.info("After: Done\n");
+        //logger.info("After: Done\n");
     }
 
-    private class getUnicalDoctor {
-
-
-    }
 }
