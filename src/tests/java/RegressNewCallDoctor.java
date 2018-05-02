6158ef515766553a77a35fc2994af6b1b5537100
Leovante <leovante@live.ru>
2018-05-02 23:02:02 +0300

    think about JavascriptExecutor

---
+++ src/tests/java/RegressNewCallDoctor.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.JavascriptExecutor;
+
+/**
+ * Created by Leovante on 5/2/2018.
+ */
+public class RegressNewCallDoctor {
+
+
+    private void waitForAngular() {
+
+/*
+        final String script =
+                "var callback = arguments[arguments.length - 1];\n" +
+                "var rootSelector = \'body\';\n" +
+                "var el = document.querySelector(rootSelector);\n" +
+                "\n" +
+                "try {\n" +
+                "    if (angular) {\n" +
+                "        window.angular.getTestability(el).whenStable(callback);\n" +
+                "    }\n" +
+                "    else {\n" +
+                "        callback();\n" +
+                "    }\n" +
+                "} catch (err) {\n" +
+                "    callback(err.message);\n" +
+                "}";
+
+        ((JavascriptExecutor) driver).executeAsyncScript(script, new Object[0]);
+*/
+    }
+
+}
