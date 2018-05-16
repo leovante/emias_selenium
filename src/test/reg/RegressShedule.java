343a78477212469edbf6300b893c0e568a19fa42
Leovante <LotusEclipse88>
2018-05-16 08:32:07 +0300

    работают все тесты

---
+++ src/test/reg/RegressShedule.java
@@ -0,0 +1,141 @@
+/*eto pattern proektirovania facad
+*echo patterni
+* singleton - bil tolko odin instance odnogo klassa po vsey sisteme
+* bilder - sobrat config testa. Sobrat' nogo url s raznimi parametrami u kazhdogo
+* bridge
+* proxy
+* factory method -
+* factory class
+*patternov mnogo i oni delyatca po tipam
+* page object
+*
+*
+*
+*
+* */
+
+import org.testng.annotations.Test;
+import pages.utilities.CleanDoctorTimeTableSQL;
+import pages.utilities.Waiter;
+
+public class RegressShedule extends TestBase {
+    CleanDoctorTimeTableSQL sql = new CleanDoctorTimeTableSQL();
+
+    @Test//KEYS 1.1
+    public void createShedule() throws InterruptedException, ClassNotFoundException {
+        driver.get("http://emias.mosreg.ru/demonstration");
+        page.loginPage().enterLoginText("admin");
+        page.loginPage().enterPasswordText("RChS2014");
+        page.loginPage().clickLoginButton();
+
+        page.homePage().manageSheduleBtn();
+        String docFullName = page.doctorMethods().getUnicalDoctor(null);
+        String secondName = page.manageShedule().getSecondName(docFullName);
+        sql.deleteShedule(secondName);
+        page.doctorMethods().selectDoctor(docFullName);
+        page.manageShedule().createShedule();
+
+        page.manageShedule().verifyDeletedShedle();
+    }
+
+    @Test//KEYS 1.2
+    public void copyShedule() throws InterruptedException, ClassNotFoundException {
+        driver.get("http://emias.mosreg.ru/demonstration");
+        page.loginPage().enterLoginText("admin");
+        page.loginPage().enterPasswordText("RChS2014");
+        page.loginPage().clickLoginButton();
+
+        page.homePage().manageSheduleBtn();
+        String firstDoctor = page.doctorMethods().getUnicalDoctor(null);
+        String secondDoctor = page.doctorMethods().getUnicalDoctor(firstDoctor);
+        String second_doctor_fam = page.manageShedule().getSecondName(secondDoctor);
+        sql.deleteShedule(second_doctor_fam);
+
+        page.doctorMethods().selectDoctor(firstDoctor);
+        page.manageShedule().createShedule();
+        page.doctorMethods().selectDoctor(secondDoctor);
+        page.manageShedule().copyShedule(firstDoctor);
+        page.doctorMethods().selectDoctor(firstDoctor);
+
+        page.manageShedule().verifyCreatedShedule();
+    }
+
+    @Test//KEYS 1.3
+    public void setNotReceiveDays() throws InterruptedException {
+        driver.get("http://emias.mosreg.ru/demonstration");
+        page.loginPage().enterLoginText("admin");
+        page.loginPage().enterPasswordText("RChS2014");
+        page.loginPage().clickLoginButton();
+
+        page.homePage().manageSheduleBtn();
+        String firstDoctor = page.doctorMethods().getUnicalDoctor(null);
+        page.manageShedule().setNotReceiveDays(firstDoctor);
+
+        page.manageShedule().verifyNotReceiveDays();
+    }
+
+    @Test//KEYS 1.4
+    public void deleteShedule() throws InterruptedException, ClassNotFoundException {
+        driver.get("http://emias.mosreg.ru/demonstration");
+        page.loginPage().enterLoginText("admin");
+        page.loginPage().enterPasswordText("RChS2014");
+        page.loginPage().clickLoginButton();
+
+        page.homePage().manageSheduleBtn();
+        String first_doctor_fullname = page.doctorMethods().getUnicalDoctor(null);
+        String first_doctor_fam = page.manageShedule().getSecondName(first_doctor_fullname);
+        String second_doctor_fullname = page.doctorMethods().getUnicalDoctor(first_doctor_fullname);
+        String second_doctor_fam = page.manageShedule().getSecondName(second_doctor_fullname);
+        sql.deleteShedule(first_doctor_fam);
+        sql.deleteShedule(second_doctor_fam);
+
+        page.doctorMethods().selectDoctor(first_doctor_fullname);
+        page.manageShedule().createShedule();
+        page.doctorMethods().selectDoctor(first_doctor_fullname);
+        page.doctorMethods().selectDoctor(second_doctor_fullname);
+        page.manageShedule().createShedule();
+
+        page.manageShedule().verifyCreatedShedule();
+        page.manageShedule().deleteShedule();
+        page.manageShedule().verifyDeletedShedle();
+    }
+
+    @Test//KEYS 1.5
+    public void surviveShedule() throws InterruptedException, ClassNotFoundException {
+        driver.get("http://emias.mosreg.ru/demonstration");
+        page.loginPage().enterLoginText("admin");
+        page.loginPage().enterPasswordText("RChS2014");
+        page.loginPage().clickLoginButton();
+
+        page.homePage().manageSheduleBtn();
+        String first_doctor_fullname = page.doctorMethods().getUnicalDoctor(null);
+        String first_doctor_fam = page.manageShedule().getSecondName(first_doctor_fullname);
+        String second_doctor_fullname = page.doctorMethods().getUnicalDoctor(first_doctor_fullname);
+        String second_doctor_fam = page.manageShedule().getSecondName(second_doctor_fullname);
+        sql.deleteShedule(first_doctor_fam);
+        sql.deleteShedule(second_doctor_fam);
+
+        page.doctorMethods().selectDoctor(first_doctor_fullname);
+        page.manageShedule().createShedule();
+        page.doctorMethods().selectDoctor(first_doctor_fullname);
+        page.doctorMethods().selectDoctor(second_doctor_fullname);
+        page.manageShedule().createShedule();
+
+        page.homePage().logoHomeBtn();
+        page.homePage().admissionScheduleBtn();
+
+        page.doctorMethods().selectDoctor(first_doctor_fullname);
+        page.admissionSchedule().createRecord(first_doctor_fullname);
+
+        driver.get("http://emias.mosreg.ru/demonstration/ScheduleWriting/Transfer");
+        Waiter.waitAllEmias();
+        first_doctor_fullname = page.doctorMethods().getUnicalDoctor(null);
+        second_doctor_fullname = page.doctorMethods().getUnicalDoctor(first_doctor_fullname);
+        page.doctorMethods().selectDoctor(first_doctor_fullname);
+        page.transferRecords().trancRecord(second_doctor_fullname);
+        page.doctorMethods().selectDoctor(first_doctor_fullname);
+        page.doctorMethods().selectDoctor(second_doctor_fullname);
+
+        page.transferRecords().verifyTransferShedule();
+    }
+}
