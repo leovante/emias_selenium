25f0e7a06fd25f9932e309fa29c125eb4b46957b
Leovante <LotusEclipse88>
2018-06-26 15:36:40 +0300

    перенес логин мис в beforeGroups

--- src/test/java/emias/BaseTest.java
+++ src/test/java/emias/BaseTest.java
@@ -21,21 +21,19 @@ public abstract class BaseTest {
 
 //    ScreenshotListener listner;
 
-    @Parameters(value = {"browser", "platform", "site", "login", "pass"})
+    @Parameters(value = {"browser", "platform"})
     @BeforeSuite(alwaysRun = true)
-    public void beforeSuite(@Optional String browser, @Optional String platform, @Optional String site, @Optional String login, @Optional String pass) throws MalformedURLException {
+    public void beforeSuite(@Optional String browser,
+                            @Optional String platform) throws MalformedURLException {
         System.out.println("Browser: " + browser);
         System.out.println("Platform: " + platform);
-        System.out.println("Site: " + site);
         driver = new DriverManager(browser).createDriver();
         JSWaiter.setDriver(driver);
         SwitchToPage.setDriver(driver);
         Waiter.setDriver(driver);
         wait = new WebDriverWait(driver, 20);
         page = new Pages(driver);
-//        listner = new ScreenshotListener(driver);
 
-        page.loginPage().login(site, login, pass);
     }
 
     @AfterSuite(alwaysRun = true)
@@ -43,10 +41,16 @@ public abstract class BaseTest {
         driver.quit();
     }
 
+    @Parameters(value = {"site", "login", "pass"})
     @BeforeGroups(groups = "CD", alwaysRun = true)
-    public void beforeGroupsCD() {
+    public void beforeGroupsCD(@Optional String site,
+                               @Optional String login,
+                               @Optional String pass) {
+        System.out.println("Site: " + site);
+        page.loginPage().login(site, login, pass);
         page.homePage().callDoctorBtn();
         pages.utilities.SwitchToPage.switchToPage();
+
 //        driver.get("http://192.168.7.19:6001/test/call/call_doctor_ui/call-doctor/board?ticket=t7zsrR0YvQRlp3lJYS0jKJNV0iik4wUMdzEgXNDtpEyZuZQrHX77ZqJ2O11UnOgX1DHriJXe6vZ%2BkgsHMnU1xMnAGoTQa0nyWMnJ6lShS7L6cGJpGGyR0M3Jlhgz1wTVz1QjW1woODUjtUE6X3KSOqtvsn3%2Fa2Lx5TDEhQG2psDKSZMl7i1%2FUVhRyDuf23%2BsEZuRtfv7ZTo%2BZ774dRCiDAC7z5deDUcuutxGMFWR%2FhwPEUS44kPBJjoJVBVZfFs58LK3FPTZL0js%2B0rNRAI%2BFt4dLcLYfI2YdpzaXFaDGrtnDRId&ReturnUrl=http:%2F%2F192.168.7.139%2Fwhc%2FMain%2FDefault");
         String url = driver.getCurrentUrl();
         curUrlCalldoctor = url;
@@ -57,8 +61,13 @@ public abstract class BaseTest {
         page.dashboardPage().exitToMis();
     }
 
+    @Parameters(value = {"site", "login", "pass"})
     @BeforeGroups(groups = "mis", alwaysRun = true)
-    public void beforeGroupsMIS() {
+    public void beforeGroupsMIS(@Optional String site,
+                                @Optional String login,
+                                @Optional String pass) {
+        System.out.println("Site: " + site);
+        page.loginPage().login(site, login, pass);
         String url = driver.getCurrentUrl();
         curUrlCalldoctor = url;
     }
@@ -72,8 +81,13 @@ public abstract class BaseTest {
     public void beforeGroupsCC() {
     }
 
+    @Parameters(value = {"site", "login", "pass"})
     @BeforeGroups(groups = "test", alwaysRun = true)
-    public void beforeGroupsCDTest() {
+    public void beforeGroupsTest(@Optional String site,
+                                 @Optional String login,
+                                 @Optional String pass) {
+        System.out.println("Site: " + site);
+        page.loginPage().login(site, login, pass);
         page.homePage().callDoctorBtn();
         pages.utilities.SwitchToPage.switchToPage();
 //        driver.get("http://192.168.7.19:6001/test/call/call_doctor_ui/call-doctor/board?ticket=t7zsrR0YvQRlp3lJYS0jKJNV0iik4wUMdzEgXNDtpEyZuZQrHX77ZqJ2O11UnOgX1DHriJXe6vZ%2BkgsHMnU1xMnAGoTQa0nyWMnJ6lShS7L6cGJpGGyR0M3Jlhgz1wTVz1QjW1woODUjtUE6X3KSOqtvsn3%2Fa2Lx5TDEhQG2psDKSZMl7i1%2FUVhRyDuf23%2BsEZuRtfv7ZTo%2BZ774dRCiDAC7z5deDUcuutxGMFWR%2FhwPEUS44kPBJjoJVBVZfFs58LK3FPTZL0js%2B0rNRAI%2BFt4dLcLYfI2YdpzaXFaDGrtnDRId&ReturnUrl=http:%2F%2F192.168.7.139%2Fwhc%2FMain%2FDefault");
