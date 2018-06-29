c37b307d64fef0e3ab35925cf20fbfdfb635106f
Leovante <leovante@live.ru>
2018-06-29 22:29:29 +0300

    Merge branch 'master' of https://github.com/leovante/emias_selenium
    # Conflicts:
    #	src/test/java/emias/calldoctor/RCD01Test.java

--- src/test/java/emias/BaseTest.java
+++ src/test/java/emias/BaseTest.java
@@ -88,7 +88,6 @@ public abstract class BaseTest {
         driver.get("http://mis.softrust.ru/mis/Main/Default");
         page.homePage().callDoctorBtn();
         pages.utilities.SwitchToPage.switchToPage();
-        driver.get("http://109.95.224.42:2165/test/call/call_doctor_ui/call-doctor;709/board?ticket=bcNzk9MEiGwEe1JSseWBq4%2BI41Qaipq6K9uyshR50QBn1zJsAobTnQ3dJZiX8p10vS3nDLSdLkIenWZRdc8rc21FIczd%2FmQe9eHSAgKxVl8C9BS18t3q3DRl%2ByMO%2FUw1EvqtO6f%2B0gWzqXBQYR%2FlPpXi9%2FIUdctXYoHD9gMJnz0q9Qbvi%2FdF1G04Z6UssA0lwE6kMYIZQgZRqHH7x%2Bvq51aCcSgYGFM1ZVh%2FnxXLcghNdJjfMCsr4FbrB7ce0nN0MWs05RY%2BADgWMmIqR5vQOgpyNB5NxOPYXN%2F5Eo1aDITM96sI&ReturnUrl=http%3A%2F%2F192.168.7.139%2Fwhc%2FMain%2FDefault");
         String url = driver.getCurrentUrl();
         curUrlCalldoctor = url;
     }
