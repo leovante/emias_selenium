d164e97a77cb53daa43c86e13d0755514e3c7ac0
Leovante <leovante@live.ru>
2018-07-19 21:37:45 +0300

    пытаюсь сделать value object профилей для тестирования. Очень важно!

--- src/test/java/emias/calldoctor/RCD00Test.java
+++ src/test/java/emias/calldoctor/RCD00Test.java
@@ -11,46 +11,33 @@ import java.util.ArrayList;
 
 public class RCD00Test extends AbstractTest {
 
-<<<<<<< .merge_file_a10924
-<<<<<<< .merge_file_a05364
-<<<<<<< .merge_file_a09164
-    @Test(groups = {"mis"}, description = "Завершаю вызовы у тестовых врачей и создаю новое расписание на сегодня")
-    @RetryCountIfFailed(2)
-    public void cleanBeforeTest() throws InterruptedException {
-        //driver.get(curUrlCalldoctor);
-=======
-=======
->>>>>>> .merge_file_a11688
-=======
->>>>>>> .merge_file_a14284
-    @Test(groups = "mis", description = "Завершаю вызовы у тестовых врачей и создаю новое расписание на сегодня")
-    @RetryCountIfFailed()
-    public void cleanBeforeCallDoctorTests() throws InterruptedException {
-        driver.get(curUrlCalldoctor);
->>>>>>> .merge_file_a04816
+        @Test(groups = "mis", description = "Завершаю вызовы у тестовых врачей и создаю новое расписание на сегодня")
+        @RetryCountIfFailed()
+        public void cleanBeforeCallDoctorTests () throws InterruptedException {
+//        driver.get(curUrlCalldoctor);
 
-        page.homePage().manageSheduleBtn();
-        createDoctorShedule();
-    }
-
-    @Step("Создаю расписание врача")
-    public void createDoctorShedule() throws InterruptedException {
-        ArrayList<String> doctors = new ArrayList<>();
-        doctors.add("Темников Дмитрий Олегович");//Юр. лицо глюченное
-        doctors.add("Серова Инна Кузьминична");//Юр лицо
-        doctors.add("Немцова Татьяна Андреевна");//Взрослая поликлиника
-
-        for (String doctor_num : doctors) {
-            String doctor_fam = ManageShedule.getSecondName(doctor_num);
-            SQLDemonstration.finalizeCallLpuDoctor(doctor_fam);
-            SQLDemonstration.deleteShedule(doctor_fam);
+            page.homePage().manageSheduleBtn();
+//        createDoctorShedule();
         }
 
-        for (String doctor_num : doctors) {
-            page.doctorMethods().selectDoctor(doctor_num);
-            page.beforeWork().createShedule();
-            page.manageShedule().verifyCreatedShedule();
-            page.doctorMethods().selectDoctor(doctor_num);
+        @Step("Создаю расписание врача")
+        public void createDoctorShedule () throws InterruptedException {
+            ArrayList<String> doctors = new ArrayList<>();
+            doctors.add("Темников Дмитрий Олегович");//Юр. лицо глюченное
+            doctors.add("Серова Инна Кузьминична");//Юр лицо
+            doctors.add("Немцова Татьяна Андреевна");//Взрослая поликлиника
+
+            for (String doctor_num : doctors) {
+                String doctor_fam = ManageShedule.getSecondName(doctor_num);
+                SQLDemonstration.finalizeCallLpuDoctor(doctor_fam);
+                SQLDemonstration.deleteShedule(doctor_fam);
+            }
+
+            for (String doctor_num : doctors) {
+                page.doctorMethods().selectDoctor(doctor_num);
+                page.beforeWork().createShedule();
+                page.manageShedule().verifyCreatedShedule();
+                page.doctorMethods().selectDoctor(doctor_num);
+            }
         }
     }
-}
