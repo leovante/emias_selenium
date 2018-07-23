3aeb7e9c65e81c0a9ef2747e93e32146ea58e038
Leovante <leovante@live.ru>
2018-07-23 19:10:48 +0300

    переделал класс проверок

--- src/test/java/emias/AbstractTest.java
+++ src/test/java/emias/AbstractTest.java
@@ -1,10 +1,7 @@
 package emias;
 
 import com.codeborne.selenide.Configuration;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.BeforeSuite;
@@ -30,20 +27,20 @@ public abstract class AbstractTest {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 //        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         System.setProperty("selenide.browser", "Chrome");
-        Configuration.startMaximized = true;
+//        Configuration.startMaximized = true;
         Configuration.browserSize = "1900x1020";
         Configuration.timeout = 10000;
         Configuration.headless = true;
         page = new Pages();
         driver = getWebDriver();
 
-        Dimension dimension = new Dimension(1920, 1080);
-        driver.manage().window().setSize(dimension);
-        driver.manage().window().maximize();
-
-        ChromeOptions option = new ChromeOptions();
-        option.addArguments("--window-size=1900,1020");
-        driver = new ChromeDriver(option);
+//        Dimension dimension = new Dimension(1920, 1080);
+//        driver.manage().window().setSize(dimension);
+//        driver.manage().window().maximize();
+//
+//        ChromeOptions option = new ChromeOptions();
+//        option.addArguments("--window-size=1900,1020");
+//        driver = new ChromeDriver(option);
     }
 
     @Parameters(value = {"site", "login", "pass"})
