04610366e3c870b1aec0a6cb9510a0fdab541c74
leovante <leovante@live.ru>
2018-08-17 17:27:27 +0300

    добавил тест

---
+++ src/main/java/pages/dispAngular/ServicesPage.java
@@ -0,0 +1,69 @@
+package pages.dispAngular;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import pages.AbstractPage;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class ServicesPage extends AbstractPage {
+    SelenideElement AllServicesTap = $(By.xpath("//*[contains(text(),'Все мероприятия')]"));
+    SelenideElement MeasureArtPressure = $(By.id("be884e59-2883-40a4-8033-4508a0ccb3ac"));
+    SelenideElement MeasureOpros_Anketirovanie = $(By.id("beccc8c1-33b3-4ed0-8c0b-22f78b792769"));
+
+    public ServicesPage() {
+
+    }
+
+    public void fillProfile1() throws InterruptedException {
+        switchAllServicesTap()
+                .fillMeasureArtPressure()
+                .fillMeasureOpros_Anketirovanie();
+    }
+
+    public ServicesPage switchAllServicesTap() {
+        AllServicesTap.click();
+        return this;
+    }
+
+    public ServicesPage fillMeasureArtPressure() throws InterruptedException {
+        Thread.sleep(700);
+        MeasureArtPressure.click();
+        Thread.sleep(700);
+        MeasureArtPressure.$(By.xpath("td[4]/mat-checkbox")).click();
+        Thread.sleep(700);
+        MeasureArtPressure.click();
+        return this;
+    }
+
+    public ServicesPage fillMeasureOpros_Anketirovanie() throws InterruptedException {
+        Thread.sleep(700);
+        MeasureOpros_Anketirovanie.hover();
+        MeasureOpros_Anketirovanie.click();
+        Thread.sleep(700);
+        MeasureOpros_Anketirovanie.$(By.xpath("../tr[3]")).$(By.xpath(".//*[contains(text(),'Анкета для граждан в возрасте до 75 лет')]")).hover();
+        MeasureOpros_Anketirovanie.$(By.xpath("../tr[3]")).$(By.xpath(".//*[contains(text(),'Анкета для граждан в возрасте до 75 лет')]")).click();
+        Thread.sleep(700);
+        // TODO: 17.08.2018 сделать переключение на iframe пока не удалось
+        //        SelenideElement frame = $(By.xpath("//iframe[@src='/test/disp/disp_ui/webmis/api/ehr/medicalrecords/95a0f83b-36d4-498e-bd10-e9d3fc3e8916/editview']"));
+        //        switchTo().frame(frame);
+        //        $(By.xpath("//*[contains(text(),'1. Говорил ли Вам врач когда-либо, что у Вас имеется')]")).hover();
+        MeasureOpros_Anketirovanie.$(By.xpath("../tr[3]")).$(By.xpath(".//tr[@id='84900']"));
+        Thread.sleep(1500);
+        MeasureOpros_Anketirovanie.$(By.xpath("../tr[3]")).$(By.xpath(".//div[@class='cursorPointer hide-med-record']")).hover();
+        MeasureOpros_Anketirovanie.$(By.xpath("../tr[3]")).$(By.xpath(".//button[contains(text(),'Просмотреть')]")).click();
+        Thread.sleep(3000);
+        MeasureOpros_Anketirovanie.$(By.xpath("../tr[3]")).$(By.xpath(".//button[@mattooltip='Добавить']")).hover();
+        MeasureOpros_Anketirovanie.$(By.xpath("../tr[3]")).$(By.xpath(".//*[contains(text(),'Сохранить')]")).click();
+        Thread.sleep(700);
+        MeasureOpros_Anketirovanie.$(By.xpath("../tr[3]")).$(By.xpath(".//*[contains(text(),'Подписать')]")).hover();
+        MeasureOpros_Anketirovanie.$(By.xpath("../tr[3]")).$(By.xpath(".//*[contains(text(),'Подписать')]")).click();
+        Thread.sleep(700);
+        MeasureOpros_Anketirovanie.$(By.xpath("./td[4]/mat-checkbox")).hover();
+        MeasureOpros_Anketirovanie.$(By.xpath("./td[4]/mat-checkbox")).click();
+        Thread.sleep(700);
+        MeasureOpros_Anketirovanie.hover();
+        MeasureOpros_Anketirovanie.click();
+        return this;
+    }
+}
--- src/test/java/emias/AbstractTest.java
+++ src/test/java/emias/AbstractTest.java
@@ -26,7 +26,7 @@ public abstract class AbstractTest {
 
     @AfterSuite(alwaysRun = true)
     public void afterSutie() {
-        driver.quit();
+//        driver.quit();
     }
 
     @Parameters(value = {"site", "login", "pass"})
@@ -53,10 +53,6 @@ public abstract class AbstractTest {
         curUrlCalldoctor = driver.getCurrentUrl();
     }
 
-    @BeforeGroups(groups = "CC", alwaysRun = true)
-    public void beforeGroupsCC() {
-    }
-
     @Parameters(value = {"login", "pass"})
     @BeforeGroups(groups = "test", alwaysRun = true)
     public void beforeGroupsTest(@Optional String login,
@@ -66,6 +62,16 @@ public abstract class AbstractTest {
         switchTo().window(1);
         curUrlCalldoctor = driver.getCurrentUrl();
     }
+
+//    @Parameters(value = {"login", "pass"})
+//    @BeforeGroups(groups = "disp", alwaysRun = true)
+//    public void beforeGroupsDisp(@Optional String login,
+//                                 @Optional String pass) {
+//        Selenide.open("http://service.emias.mosreg.ru/test/disp/disp_ui/disp;doctorId=1208;doctorGuid=20d0da4b-b333-4878-9409-4525eae8e502;doctorTypeGuid=81d86a3b-2c5a-44b0-8ef9-48e34fbce21d;ticket=RqBrz85637XYrV2zFG8eZvYGvZiHSrF94TOHue0ltIZdapgZgXkv4sQcM0xD7g8soyDqwjLgeUQXu2Vd1n0XMEZyhoPIDVOEHbG%2FEmzG3oHGtBDPonMq%2FJFCnLoePrGqFQ0ah5DBFRv9zYEg%2BHYqwbvHeRVOHhz6Q9mQXyfsKTuC12xh6wLYRogVQi2KwEJq%2F5uAlC8vLHVZnBR7dujH2rB%2BEBN4tUwINqV1UE8CxcH82u54CO00eC84C8tkPBgbip4LJrEnHUz7TZj1n0HvU1mQHOSA5RbY5xgUnRJE5O0vFzpf/card/1837?ticket=RqBrz85637XYrV2zFG8eZvYGvZiHSrF94TOHue0ltIZdapgZgXkv4sQcM0xD7g8soyDqwjLgeUQXu2Vd1n0XMEZyhoPIDVOEHbG%2FEmzG3oHGtBDPonMq%2FJFCnLoePrGqFQ0ah5DBFRv9zYEg%2BHYqwbvHeRVOHhz6Q9mQXyfsKTuC12xh6wLYRogVQi2KwEJq%2F5uAlC8vLHVZnBR7dujH2rB%2BEBN4tUwINqV1UE8CxcH82u54CO00eC84C8tkPBgbip4LJrEnHUz7TZj1n0HvU1mQHOSA5RbY5xgUnRJE5O0vFzpf&mkabId=0&dvtId=376453&docPrvdId=1208&MisUrl=http:%2F%2Femias.mosreg.ru%2Fdemonstration&ReturnUrl=http:%2F%2Femias.mosreg.ru%2Fdemonstration%2FSchedule");
+////        page.homePage().callDoctorBtn();
+////        switchTo().window(1);
+////        curUrlCalldoctor = driver.getCurrentUrl();
+//    }
 
 //    public static void disableWarning() {
 //        try {
