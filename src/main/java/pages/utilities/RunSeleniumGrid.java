e6a15bcb63275f6ceebda703b25017b73b03113d
Leovante <leovante@live.ru>
2018-09-14 23:47:34 +0300

    правки

--- src/main/java/pages/utilities/RunSeleniumGrid.java
+++ src/main/java/pages/utilities/RunSeleniumGrid.java
@@ -5,8 +5,15 @@ import java.io.IOException;
 public class RunSeleniumGrid {
 
     public static void run() throws IOException {
-        Runtime.
-                getRuntime().
-                exec("src/main/resources/run_grid.bat");
+//        Runtime.
+//                getRuntime().
+//                exec("src/main/resources/run_grid.bat");
+
+        ProcessBuilder pb = new ProcessBuilder();
+        pb.command("java ", "-jar ", "src/main/resources/selenium-server-standalone-3.14.0.jar ", "-role hub", "-hubConfig src/main/resources/hubconfig.json");
+//        pb.command("java", "-jar", "src/main/resources/selenium-server-standalone-3.14.0.jar", "-role node", "-nodeConfig nodeconfig.json");
+        Process p = pb.start();
+
+        System.out.println("Запустил Selenium Grid");
     }
 }
--- src/main/resources/run_grid.bat
+++ src/main/resources/run_grid.bat
@@ -1,2 +1,3 @@
+@ECHO OFF
 start/MIN  java -jar selenium-server-standalone-3.14.0.jar -role hub -hubConfig hubconfig.json
 start/MIN java -jar selenium-server-standalone-3.14.0.jar -role node -nodeConfig nodeconfig.json
