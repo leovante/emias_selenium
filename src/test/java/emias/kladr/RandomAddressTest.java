332b7eb85e1aad38ad42dfe32858b7937df0736f
Dmitry Temnikov <handleactionjavaevent@ya.ru>
2018-11-13 18:09:20 +0300

    правки

--- src/test/java/emias/kladr/RandomAddressTest.java
+++ src/test/java/emias/kladr/RandomAddressTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 
 public class RandomAddressTest extends AbstractTestGrid {
 
-    @Test(groups = "kl", description = "рандомный вызов по кладр")
+    @Test(groups = "kl", description = "рандомный вызов по кладр", enabled = false)
     @Epic("Кладр")
     @RetryCountIfFailed(2)
     public void testCancelCallFrom_Registr() {
