fe441096eb643354a1e2f1d55c8200d721e1828b
Dmitry Temnikov <handleactionjavaevent@ya.ru>
2019-04-22 13:22:32 +0300

    правка

--- src/test/java/emias/disp/regress/RouteListTest.java
+++ src/test/java/emias/disp/regress/RouteListTest.java
@@ -50,7 +50,7 @@ public class RouteListTest extends TestBase {
     }
     // TODO: 14-Feb-19 сделать тест добавление мероприятия на истекшее время. Бекетова 63 года
 
-    @Test(groups = "disp", description = "подписываем мероприятие без маркировок")
+    @Test(groups = "disp", description = "отображение кнопки просмотреть у мероприятия без маркировок. И подписание")
     @RetryCountIfFailed(2)
     public void validateKallMeasure() throws InterruptedException {
         open("http://192.168.7.24/test/disp;doctorId=2124;doctorGuid=c46f9814-ca4e-4783-af14-cf3278deb0aa;doctorTypeGuid=81d86a3b-2c5a-44b0-8ef9-48e34fbce21d;ticket=4mv%2FQ%2FIyd7PZ5BEqmxjKPtBE7pOFs%2B7Ly51c67wldzPHmnsQt0Ijyo4sY3HeZMk0PvogiIMSgidunJj0MlSNlq0682MswdMHtdWWYGdXxeae6borusURm3SnOWUtO3StxBFV%2Fj87TzEAEnCYzk1cBOl4yOrzbtO%2Bs9bUMvnZM3Q6W0aTHddskTa5pEv6Ttt3Ig4aZhUWjQ64OK3O3owuJ9wycmA59ShmhsLFs8SM2VTBMiYmWvb8GYe86Bta1rXPdvNUQCiL25wbK8fYvivygGg4SiNGowHMbCjlLvbkCYUmdWEInRq9gAcMkGQ5I3Z0SMDfUA%3D%3D/card/467?ticket=4mv%2FQ%2FIyd7PZ5BEqmxjKPtBE7pOFs%2B7Ly51c67wldzPHmnsQt0Ijyo4sY3HeZMk0PvogiIMSgidunJj0MlSNlq0682MswdMHtdWWYGdXxeae6borusURm3SnOWUtO3StxBFV%2Fj87TzEAEnCYzk1cBOl4yOrzbtO%2Bs9bUMvnZM3Q6W0aTHddskTa5pEv6Ttt3Ig4aZhUWjQ64OK3O3owuJ9wycmA59ShmhsLFs8SM2VTBMiYmWvb8GYe86Bta1rXPdvNUQCiL25wbK8fYvivygGg4SiNGowHMbCjlLvbkCYUmdWEInRq9gAcMkGQ5I3Z0SMDfUA%3D%3D&mkabId=0&dvtId=1467230&docPrvdId=2124&MisUrl=http:%2F%2F192.168.7.54%2Ftest&ReturnUrl=http:%2F%2F192.168.7.54%2Ftest%2FSchedule");
@@ -60,6 +60,4 @@ public class RouteListTest extends TestBase {
                 .signService()
                 .validateServiceIsSign();
     }
-
-    // TODO: 4/17/2019 проверить отображение кнопки просмотреть у мероприятия исследование кала на скрытую кровь
 }
