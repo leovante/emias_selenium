da591899028a9786860ed575eb17e5b08e8d5a7e
Dmitry Temnikov <handleactionjavaevent@ya.ru>
2019-07-02 17:45:56 +0300

    правки

--- src/main/java/com/utils/WebDriverInstansiator.java
+++ src/main/java/com/utils/WebDriverInstansiator.java
@@ -50,7 +50,7 @@ public class WebDriverInstansiator {
             driver = new ChromeDriver(chromeOptions);
             driver.manage().window().setPosition(new Point(0, 0));
             WebDriverRunner.setWebDriver(driver);
-            Configuration.timeout = 30000;
+            Configuration.timeout = 20000;
             Configuration.savePageSource = false;
         }
         //селениум грид
