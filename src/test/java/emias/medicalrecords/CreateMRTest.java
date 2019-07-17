6be57bfb235e6e62c39ceb5d651cfaaa814b8725
Dmitry Temnikov <handleactionjavaevent@ya.ru>
2019-07-17 17:17:39 +0300

    Медзаписи. Подправил тесты подписания старых записей и новых шаблонов [правки]

--- src/test/java/emias/medicalrecords/CreateMRTest.java
+++ src/test/java/emias/medicalrecords/CreateMRTest.java
@@ -21,7 +21,7 @@ public class CreateMRTest extends TestBase {
         open("http://192.168.7.24/test/mr;master=true;doctorId=2135;doctorGuid=24688d3b-5db6-4ce7-823e-50c6ba025a8d;doctorTypeGuid=81D86A3B-2C5A-44B0-8EF9-48E34FBCE21D;ticket=7Hqk2aACn3jcvP8jXPJvIlv1dDL90dDfuOmd0IF1mjjd8m3VZvSw5nGbVLHMy1AYzmkOsPoeCNnk9S6ySFFn0O8nEKEMxUdBCDbkA%2Bxj2h4tnPGaO3V7huXbtXgBt04Ag2kilQSP71fHF0%2BFFQocSnXggq3yk%2B8bVXMSxdNelFvWLsVWLX%2B%2BDJpA7iGgNRrXSVz6erw%2Fex8w0zsm48FuIvobrIfmNpFJq4fvTBJOCyyERE%2F4UCMyxnJQOF3LjzShVEiZyeKLa7o1BPBgmUi%2BEv4JRJ8zW6fS9leKR%2Fj6nHg73FWgmRBzJ9jjWcmcVyAn4WjFmw%3D%3D/patient/2662111/59b63737-77c2-4dca-b4dd-b543d5db175d/E804E486-D7A8-47A1-870D-56BA14D67AB8/event/2664807/6ff738d2-f9d5-4423-bf58-d01c390d22d9/523452F6-124E-4D63-94C4-012D71072FD3/medicalrecords?ticket=7Hqk2aACn3jcvP8jXPJvIlv1dDL90dDfuOmd0IF1mjjd8m3VZvSw5nGbVLHMy1AYzmkOsPoeCNnk9S6ySFFn0O8nEKEMxUdBCDbkA%2Bxj2h4tnPGaO3V7huXbtXgBt04Ag2kilQSP71fHF0%2BFFQocSnXggq3yk%2B8bVXMSxdNelFvWLsVWLX%2B%2BDJpA7iGgNRrXSVz6erw%2Fex8w0zsm48FuIvobrIfmNpFJq4fvTBJOCyyERE%2F4UCMyxnJQOF3LjzShVEiZyeKLa7o1BPBgmUi%2BEv4JRJ8zW6fS9leKR%2Fj6nHg73FWgmRBzJ9jjWcmcVyAn4WjFmw%3D%3D&DocPrvdId=2135");
         $x("//*[contains(text(),'Новая медицинская запись')]").click();
         $x("//*[contains(text(),'Все шаблоны')]").click();
-        $x("//*[@placeholder = 'Поиск шаблона по номеру, наименованию и специализации']").setValue("гастро");
+        $x("//*[@placeholder = 'Поиск шаблона по номеру, наименованию и специализации']").setValue("Осмотр гастроэнтеролога");
         $x("//i[@class = 'zmdi zmdi-search']").click();
         $x("//span[@title = 'Осмотр гастроэнтеролога']").doubleClick();
         $x("//*[contains(text(),'Просмотреть')]").click();
@@ -36,7 +36,7 @@ public class CreateMRTest extends TestBase {
         open("http://192.168.7.24/test/mr;master=true;doctorId=2135;doctorGuid=24688d3b-5db6-4ce7-823e-50c6ba025a8d;doctorTypeGuid=81D86A3B-2C5A-44B0-8EF9-48E34FBCE21D;ticket=7Hqk2aACn3jcvP8jXPJvIlv1dDL90dDfuOmd0IF1mjjd8m3VZvSw5nGbVLHMy1AYzmkOsPoeCNnk9S6ySFFn0O8nEKEMxUdBCDbkA%2Bxj2h4tnPGaO3V7huXbtXgBt04Ag2kilQSP71fHF0%2BFFQocSnXggq3yk%2B8bVXMSxdNelFvWLsVWLX%2B%2BDJpA7iGgNRrXSVz6erw%2Fex8w0zsm48FuIvobrIfmNpFJq4fvTBJOCyyERE%2F4UCMyxnJQOF3LjzShVEiZyeKLa7o1BPBgmUi%2BEv4JRJ8zW6fS9leKR%2Fj6nHg73FWgmRBzJ9jjWcmcVyAn4WjFmw%3D%3D/patient/2662111/59b63737-77c2-4dca-b4dd-b543d5db175d/E804E486-D7A8-47A1-870D-56BA14D67AB8/event/2664807/6ff738d2-f9d5-4423-bf58-d01c390d22d9/523452F6-124E-4D63-94C4-012D71072FD3/medicalrecords?ticket=7Hqk2aACn3jcvP8jXPJvIlv1dDL90dDfuOmd0IF1mjjd8m3VZvSw5nGbVLHMy1AYzmkOsPoeCNnk9S6ySFFn0O8nEKEMxUdBCDbkA%2Bxj2h4tnPGaO3V7huXbtXgBt04Ag2kilQSP71fHF0%2BFFQocSnXggq3yk%2B8bVXMSxdNelFvWLsVWLX%2B%2BDJpA7iGgNRrXSVz6erw%2Fex8w0zsm48FuIvobrIfmNpFJq4fvTBJOCyyERE%2F4UCMyxnJQOF3LjzShVEiZyeKLa7o1BPBgmUi%2BEv4JRJ8zW6fS9leKR%2Fj6nHg73FWgmRBzJ9jjWcmcVyAn4WjFmw%3D%3D&DocPrvdId=2135");
         $x("//*[contains(text(),'Новая медицинская запись')]").click();
         $x("//*[contains(text(),'Все шаблоны')]").click();
-        $x("//*[@placeholder = 'Поиск шаблона по номеру, наименованию и специализации']").setValue("гастро");
+        $x("//*[@placeholder = 'Поиск шаблона по номеру, наименованию и специализации']").setValue("Консультация врача гастроэнтеролога");
         $x("//i[@class = 'zmdi zmdi-search']").click();
         $x("//span[@title = 'Консультация врача гастроэнтеролога 2']").doubleClick();
         $x("//*[contains(text(),'Просмотреть')]").hover().click();
